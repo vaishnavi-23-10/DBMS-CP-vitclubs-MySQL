808c3bae6a6564852f26abc9e37fb1fb0afe013b
PhantomFury7 <adithyaumare@gmail.com>
2023-09-02 23:44:00 +0530

    Adithya added ISA table and entries.

--- clubs.sql
+++ clubs.sql
@@ -1,1 +1,14 @@
-testing 123
+create table ISA (
+club_name varchar (50) default "ISA", 
+PNR INT (8),
+first_name varchar (50),
+last_name varchar (50),
+year_name varchar (10),
+domain_name varchar (50)
+);
+insert into isa (PNR, first_name, last_name, year_name, domain_name)
+values 	(12210001, "Raj", "Kumar", "S.Y.", "Technical"),
+		(12210002, "Suraj", "Gupta", "S.Y.", "Technical"),
+        (12210003, "Rishi", "Agrawal", "S.Y.", "Technical"),
+        (12110001, "Rohit", "Chauhan", "T.Y.", "Management"),
+        (12110002, "Nilesh", "Rathod", "T.Y.", "Management");
