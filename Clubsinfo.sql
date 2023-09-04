83ea946a469c09d55eaab3955962892b572f8f27
vaishnavi_singh <119932034+vaishnavi-23-10@users.noreply.github.com>
2023-09-04 19:11:27 +0530

    gdsc entries added

--- Clubsinfo.sql
+++ Clubsinfo.sql
@@ -5,3 +5,8 @@
 	Name varchar(40),
     Branch varchar(40),
     positon varchar(40));
+    
+insert into gdsc (DomainID,DomainName,PRN,Name,Branch,positon) values 
+    ("L01","Technical",12210100,"Neha","CS","Head"),
+    ("L02","Technical",12210123,"Rahul","Chemical","Secretary"),
+    ("L03","Technical",12210465,"Vaishnavi","CS-AIML","Secretary");
