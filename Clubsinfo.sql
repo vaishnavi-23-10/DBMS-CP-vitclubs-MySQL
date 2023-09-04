2ab6ed4105f027e64481f7a82e332447926f419f
vaishnavi_singh <119932034+vaishnavi-23-10@users.noreply.github.com>
2023-09-04 18:59:25 +0530

    code pushed

---
+++ Clubsinfo.sql
@@ -0,0 +1,7 @@
+ create table gdsc(
+	DomainID int NOT NULL,
+	DomainName varchar(40),
+    PRN int,
+	Name varchar(40),
+    Branch varchar(40),
+    positon varchar(40));
--- README.md
+++
@@ -1,2 +0,0 @@
-# DBMS-CP-vitclubs-MySQL
-Course Project Code for Database Management System in MySQL
